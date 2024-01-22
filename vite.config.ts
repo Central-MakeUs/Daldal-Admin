2752831e4227fc1df588f4e71c3a1d509e85fe5b
ShinYoung-Kim <lukey0515@naver.com>
2024-01-22 11:37:04 +0900

    chore: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,30 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: '@components',
+				replacement: '/src/components',
+			},
+			{ find: '@hooks', replacement: '/src/hooks' },
+			{ find: '@pages', replacement: '/src/pages' },
+			{ find: '@styles', replacement: '/src/styles' },
+			{ find: '@utils', replacement: '/src/utils' },
+			{ find: '@type', replacement: '/src/type' },
+			{ find: '@assets', replacement: '/src/assets' },
+			{
+				find: '@constants',
+				replacement: '/src/constants',
+			},
+			{ find: '@stores', replacement: '/src/stores' },
+			{ find: '@models', replacement: '/src/models' },
+			{ find: '@contexts', replacement: '/src/contexts' },
+			{ find: '@mocks', replacement: '/src/mocks' },
+			{ find: '@tests', replacement: '/src/tests' },
+			{ find: '@apis', replacement: '/src/apis' },
+			{ find: '@layouts', replacement: '/src/layouts' },
+			{ find: '@', replacement: '/src' },
+		],
+	},
 });
