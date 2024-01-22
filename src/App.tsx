761cf9036c9563a5658e7bb7e3e34f76fb096cb6
ShinYoung-Kim <lukey0515@naver.com>
2024-01-22 12:02:55 +0900

    chore: react query devtools 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>Hello World</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			Hello World
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
