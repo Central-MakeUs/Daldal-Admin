22cb9c21f72be477d19d4816e4e54072d39efe2a
ShinYoung-Kim <lukey0515@naver.com>
2024-01-28 15:06:19 +0900

    feat: 상수 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+	Black: '#17181A',
+	White: '#FFFFFF',
+	Primary: '#FF617A',
+	Secondary_B: '#75D6FF',
+	Secondary_G: '#C1F78D',
+	Kakao: '#FBE44D',
+	Gray90: '#272A31',
+	Gray80: '#414651',
+	Gray70: '#5C6271',
+	Gray60: '#7C8393',
+	Gray50: '#979FB3',
+	Gray20: '#BBC2D2',
+	Gray10: '#D8DEED',
+	Error: '#FF8989',
+} as const;
+
+export default colors;
