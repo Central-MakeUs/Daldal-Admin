c71cdacbd555101c1c8d72d1aec5c2c21b21023c
ShinYoung-Kim <lukey0515@naver.com>
2024-01-31 20:42:16 +0900

    feat: 메인 페이지 구성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import ButtonWithIconGroup from '@components/molcules/main/ButtonWithIconGroup';
+import MainTable from '@components/templates/MainTable';
 import PageLayout from '@layouts/PageLayout';
 
 const Main = () => {
 	return (
 		<PageLayout>
 			<ButtonWithIconGroup />
+			<MainTable />
 		</PageLayout>
 	);
 };
