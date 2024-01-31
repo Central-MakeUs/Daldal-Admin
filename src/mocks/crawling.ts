439fa2a94929eb88c627f4d79382aa6d224aa8cf
ShinYoung-Kim <lukey0515@naver.com>
2024-02-01 01:24:24 +0900

    feat: 크롤링 mock 데이터 수정

--- src/mocks/crawling.ts
+++ src/mocks/crawling.ts
@@ -1,33 +1,30 @@
-export const crawlingReadOnlyMockData = Array(10)
-	.fill(undefined)
-	.map((_, index) => ({
-		id: index,
-		detail: '상품 상세 설명',
-		thumbnailUrl: 'https://via.placeholder.com/150',
-		categoryType: '카테고리',
-		imageUrls: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		videoUrls: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		redirectUrl: 'https://via.placeholder.com/150',
-		isMDPick: true,
-		isOpen: true,
-		price: 10000,
-	}));
+import { CrawlingTableData } from '@models/crawling/entity/crawling';
 
-export const crawlingWritableData = Array(10)
+export const crawlingTableMockData: (
+	id: number | 'new',
+) => CrawlingTableData = (id: number | 'new') => ({
+	id: id === 'new' ? Math.random() : id,
+	detail: '상품 상세 설명',
+	thumbnailUrl: 'https://via.placeholder.com/150',
+	categoryType: '카테고리',
+	imageUrls: [
+		'https://via.placeholder.com/150',
+		'https://via.placeholder.com/150',
+	],
+	videoUrls: [
+		'https://via.placeholder.com/150',
+		'https://via.placeholder.com/150',
+	],
+	redirectUrl: 'https://via.placeholder.com/150',
+	isMDPick: true,
+	isOpen: true,
+	price: 10000,
+});
+
+export const crawlingTableMockDataList = Array(10)
 	.fill(undefined)
-	.map((_, index) => ({
-		id: index,
-		redirectUrl: 'https://via.placeholder.com/150',
-		videoUrls: [
-			'https://via.placeholder.com/150',
-			'https://via.placeholder.com/150',
-		],
-		isMDPick: false,
-		isOpen: false,
-	}));
+	.map((_, index) => crawlingTableMockData(index));
+
+export const crawlingProductLink = (id: number | 'new') => {
+	return crawlingTableMockData(id);
+};
