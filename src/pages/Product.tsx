c6aba48b36d1f2ef9d8824d13ce54b2a0c279b17
ShinYoung-Kim <lukey0515@naver.com>
2024-02-02 22:15:15 +0900

    feat: product 페이지에 section 연결

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,10 +1,18 @@
-import Title from '@components/atoms/Title';
+import AllUserSection from '@components/organisms/product/AllUserSection';
+import RequestUserSection from '@components/organisms/product/RequestUserSection';
 import PageLayout from '@layouts/PageLayout';
+import { userTableMockDataList } from '@mocks/user';
 
 const Product = () => {
+	// TODO: 데이터 get api 연결
+	const datas = userTableMockDataList;
+
 	return (
 		<PageLayout>
-			<Title title="상품 페이지" />
+			<div className="h-full flex flex-col justify-between">
+				<RequestUserSection datas={datas} />
+				<AllUserSection datas={datas} />
+			</div>
 		</PageLayout>
 	);
 };
