2ccbb34e029f259d247463cbd0b66bf2c06f4ea9
ShinYoung-Kim <lukey0515@naver.com>
2024-02-02 20:44:50 +0900

    feat: 환급 요청 유저 영역 생성

---
+++ src/components/organisms/product/RequestUserSection.tsx
@@ -0,0 +1,11 @@
+import Title from '@components/atoms/Title';
+
+const RequestUserSection = () => {
+	return (
+		<div>
+			<Title title="환급 요청 유저" />
+		</div>
+	);
+};
+
+export default RequestUserSection;
