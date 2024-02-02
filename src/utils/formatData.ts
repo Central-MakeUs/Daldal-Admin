de60032911dc6b3f74d880458f9eeeee340385f8
ShinYoung-Kim <lukey0515@naver.com>
2024-02-02 22:16:12 +0900

    feat: 표 데이터 format 함수 isRefund 추가

--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -1,13 +1,15 @@
-import { TableDataKey } from '@type/table';
+import { TableDataKey, TableDataValue } from '@type/table';
 
 export const getFormattedTableData = (
 	headerKey: TableDataKey,
-	value: string,
+	value: TableDataValue,
 ) => {
 	switch (headerKey) {
 		case 'isOpen':
 			return '보기';
 		case 'isMDPick':
+			return value === 'true' ? 'Y' : 'N';
+		case 'isRefund':
 			return value === 'true' ? 'Y' : 'N';
 		default:
 			return value;
