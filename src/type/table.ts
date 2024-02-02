59f1ffcfb8402a6bc58db73fc7d9b062b545163a
ShinYoung-Kim <lukey0515@naver.com>
2024-02-02 22:15:03 +0900

    feat: 테이블 타입 수정

--- src/type/table.ts
+++ src/type/table.ts
@@ -1,11 +1,20 @@
 import {
 	CrawlingTableData,
+	CrawlingTableDataKey,
 	CrawlingTableDataList,
+	CrawlingTableDataValue,
 } from '@models/crawling/entity/crawling';
+import {
+	UserTableData,
+	UserTableDataKey,
+	UserTableDataList,
+	UserTableDataValue,
+} from '@models/user/entity/user';
 
-export type TableDataList = CrawlingTableDataList;
-export type TableData = CrawlingTableData;
+export type TableDataList = CrawlingTableDataList | UserTableDataList;
+export type TableData = CrawlingTableData | UserTableData;
 
-export type TableDataKey = keyof TableData;
+export type TableDataKey = CrawlingTableDataKey | UserTableDataKey;
+export type TableDataValue = CrawlingTableDataValue | UserTableDataValue;
 
 export type TableDataId = number | 'new';
