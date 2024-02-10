9dae06b42760a4d21cadee0ed25804883e59b860
ShinYoung-Kim <lukey0515@naver.com>
2024-02-10 23:03:50 +0900

    feat: 메인 페이지 pagination 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,42 @@
+import { useParams } from 'react-router-dom';
+
 import AddRowButton from '@components/molcules/main/AddRowButton';
 import MainTable from '@components/templates/MainTable';
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from '@components/ui/pagination';
 import PageLayout from '@layouts/PageLayout';
 
 const Main = () => {
+	const params = useParams();
+	const page = params.page || 1;
+
 	return (
 		<PageLayout>
 			<AddRowButton />
 			<MainTable />
+			<Pagination>
+				<PaginationContent>
+					<PaginationItem>
+						<PaginationPrevious>Previous</PaginationPrevious>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href={`${page}`}>{page}</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationNext>Next</PaginationNext>
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
 		</PageLayout>
 	);
 };
