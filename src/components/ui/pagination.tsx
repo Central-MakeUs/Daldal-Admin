5897b23ef7cdf29a3df29a889c6804398eb276f8
ShinYoung-Kim <lukey0515@naver.com>
2024-02-11 01:33:55 +0900

    fix: lint 오류 해결

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,7 +1,8 @@
 import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react';
 import * as React from 'react';
 
-import { ButtonProps, buttonVariants } from '@components/ui/button';
+import { ButtonProps } from '@components/ui/button';
+import { buttonVariants } from '@constants/shadcn';
 import { cn } from '@utils/shadcn';
 
 const Pagination = ({ className, ...props }: React.ComponentProps<'nav'>) => (
