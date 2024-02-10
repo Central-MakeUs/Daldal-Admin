73d1aa0dfcdee45501a1eec246f4ba8f323db6c3
ShinYoung-Kim <lukey0515@naver.com>
2024-02-11 00:05:02 +0900

    feat: crawling post 요청 api 구현

--- src/apis/item.ts
+++ src/apis/item.ts
@@ -2,15 +2,26 @@ import { AdminItemsResponseDTO } from '@models/crawling/response/adminItemsRespo
 import { ApiResponse } from '@type/apiResponse';
 
 export const getAdminItems = async (
-	page?: number,
+	page: number,
 ): Promise<ApiResponse<AdminItemsResponseDTO>> => {
 	const response = await fetch(
-		`https://daldal.karmapol.link/api/v1/admin/items?page=${page || 1}`,
+		`https://daldal.karmapol.link/api/v1/admin/items?page=${page}`,
 	).then(res => res.json());
 
 	// if (!response.ok) {
 	// 	throw new Error('Failed to fetch');
 	// }
+
+	return response;
+};
+
+export const crawlAdminItems = async (url: string) => {
+	const response = await fetch(
+		`https://daldal.karmapol.link/api/v1/admin/items/crawl?url=${url}`,
+		{
+			method: 'POST',
+		},
+	).then(res => res.json());
 
 	return response;
 };
