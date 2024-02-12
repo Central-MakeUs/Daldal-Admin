342f4b788e5b3f23c7ef720853bb143514382356
ShinYoung-Kim <lukey0515@naver.com>
2024-02-12 23:25:14 +0900

    feat: 기존 api 코드 axios로 변경

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,16 +1,13 @@
+import { api } from '@hooks/apis';
 import { UserResponseDTO } from '@models/user/response/userResponseDTO';
 import { ApiResponse } from '@type/apiResponse';
 
 export const getUsers = async (
 	page: number,
 ): Promise<ApiResponse<UserResponseDTO>> => {
-	const response = await fetch(
+	const response = await api.get(
 		`https://daldal.karmapol.link/api/v1/admin/buys/{buyId}/withdraw?page=${page}`,
-	).then(res => res.json());
+	);
 
-	// if (!response.ok) {
-	// 	throw new Error('Failed to fetch');
-	// }
-
-	return response;
+	return response.data;
 };
