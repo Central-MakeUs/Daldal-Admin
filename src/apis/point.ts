9b9aa5607452c17360a6220ad196864ec84f8821
ShinYoung-Kim <lukey0515@naver.com>
2024-02-13 01:03:50 +0900

    feat: point api에 axios 반영

--- src/apis/point.ts
+++ src/apis/point.ts
@@ -1,3 +1,4 @@
+import { api } from '@hooks/apis';
 import { PointsResponseDTO } from '@models/point/entity/response/pointResponseDTO';
 import { ApiResponse } from '@type/apiResponse';
 import { TableDataId } from '@type/table';
@@ -5,35 +6,31 @@ import { TableDataId } from '@type/table';
 export const getPoints = async (
 	page: number,
 ): Promise<ApiResponse<PointsResponseDTO>> => {
-	const response = await fetch(
+	const response = await api.get(
 		`https://daldal.karmapol.link/api/v1/admin/buys?page=${page}`,
-	).then(res => res.json());
+	);
 
-	return response;
+	return response.data;
 };
 
 export const rejectPoint = async (id: TableDataId, rejectReason: string) => {
-	const response = await fetch(
+	const response = await api.patch(
 		`https://daldal.karmapol.link/api/v1/admin/buys/${id}/reject?buyId=${id}`,
+		JSON.stringify({ rejectReason }),
 		{
-			method: 'PATCH',
-			body: JSON.stringify({ rejectReason }),
 			headers: {
 				'Content-Type': 'application/json',
 			},
 		},
-	).then(res => res.json());
+	);
 
-	return response;
+	return response.data;
 };
 
 export const approvePoint = async (id: TableDataId) => {
-	const response = await fetch(
+	const response = await api.patch(
 		`https://daldal.karmapol.link/api/v1/admin/buys/${id}/approve?buyId=${id}`,
-		{
-			method: 'PATCH',
-		},
-	).then(res => res.json());
+	);
 
-	return response;
+	return response.data;
 };
