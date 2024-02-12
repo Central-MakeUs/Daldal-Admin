2408c2463678ede2742abe9aaf03a23808038125
ShinYoung-Kim <lukey0515@naver.com>
2024-02-12 09:43:23 +0900

    feat: page hook 적용

--- src/components/molcules/main/AddRowButton.tsx
+++ src/components/molcules/main/AddRowButton.tsx
@@ -1,15 +1,18 @@
 import { useQueryClient } from '@tanstack/react-query';
 
 import ButtonWithIcon from '@components/atoms/ButtonWithIcon';
+import { useGetPageNumber } from '@hooks/page';
 import { AdminItemsResponseDTO } from '@models/crawling/response/adminItemsResponseDTO';
 import { defaultCrawlingData } from '@stores/tableData';
 import { ApiResponse } from '@type/apiResponse';
 
 const AddRowButton = () => {
 	const queryClient = useQueryClient();
+	const page = useGetPageNumber();
+
 	const handleAddColumns = () => {
 		queryClient.setQueryData(
-			['adminItems'],
+			['adminItems', page],
 			(prev: ApiResponse<AdminItemsResponseDTO>) => ({
 				...prev,
 				data: {
