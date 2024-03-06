169531ebd8b88a76622b34eb14e213dee8abb330
ShinYoung-Kim <lukey0515@naver.com>
2024-03-06 23:20:10 +0900

    feat: 메인 페이지 w-max 속성 제거

--- src/constants/columns.ts
+++ src/constants/columns.ts
@@ -1,70 +1,70 @@
 import { TableColumnList } from '@type/table';
 
 export const crawlingReadOnlyTableColumns: TableColumnList = [
-	{ name: '상품명', key: 'title', style: 'w-full ' },
-	{ name: '상품 가격', key: 'price', style: 'w-max-[141px] w-full ' },
+	{ name: '상품명', key: 'title', style: 'w-full' },
+	{ name: '상품 가격', key: 'price', style: 'w-[141px]' },
 	{
 		name: '대표 이미지',
 		key: 'thumbnailUrl',
-		style: 'w-max-[142px] w-full ',
+		style: 'w-[142px]',
 	},
 	{
 		name: '세부 이미지',
 		key: 'imageUrls',
-		style: 'w-max-[142px] w-full ',
+		style: 'w-[142px]',
 	},
 	{
 		name: '카테고리',
 		key: 'categoryType',
-		style: 'w-max-[142px] w-full ',
+		style: 'w-[142px] ',
 	},
-	{ name: '유튜브', key: 'videoUrls', style: 'w-max-[142px] w-full ' },
+	{ name: '유튜브', key: 'videoUrls', style: 'w-[142px] ' },
 ];
 
 export const crawlingWritableTableColumns: TableColumnList = [
 	{
 		name: '상품링크',
 		key: 'redirectUrl',
-		style: 'w-max-[125px] w-full ',
+		style: 'w-[125px] ',
 		isEditable: true,
 	},
 	{
 		name: '유튜브링크',
 		key: 'videoUrls',
-		style: 'w-max-[125px] w-full ',
+		style: 'w-[125px] ',
 		isEditable: true,
 	},
 	{
 		name: 'Y/N',
 		key: 'isSuggested',
-		style: 'w-max-[125px] w-full ',
+		style: 'w-[125px] ',
 		isEditable: true,
 	},
 	{
 		name: '보기',
 		key: 'isOpen',
-		style: 'w-max-[125px] w-full ',
+		style: 'w-[125px] ',
 		isClickPossible: true,
 	},
 	{
 		name: '삭제',
 		key: 'delete',
-		style: 'w-max-[125px] w-full ',
+		style: 'w-[125px] ',
 		isClickPossible: true,
 	},
 ];
 
 export const userTableColumns: TableColumnList = [
-	{ name: '유저명', key: 'depositor', style: 'max-w-[163px] w-full' },
-	{ name: '구매액', key: 'purchase', style: 'max-w-[274px] w-full' },
-	{ name: '보유 포인트', key: 'refund', style: 'max-w-[292px] w-full' },
+	{ name: '유저명', key: 'depositor', style: 'w-[163px]' },
+	{ name: '구매액', key: 'purchase', style: 'w-[274px]' },
+	{ name: '보유 포인트', key: 'refund', style: 'w-[292px]' },
 	{ name: '은행명', key: 'accountBank', style: 'w-full' },
-	{ name: '계좌번호', key: 'account', style: 'max-w-[350px] w-full' },
+	{ name: '계좌번호', key: 'account', style: 'w-[350px]' },
 	{ name: '환급요청시기', key: 'uploadTime', style: 'w-full' },
 	{
 		name: '환급여부',
 		key: 'refundStatus',
-		style: 'max-w-[168px] w-full',
+		style: 'w-[168px]',
 		isEditable: true,
 	},
 ];
